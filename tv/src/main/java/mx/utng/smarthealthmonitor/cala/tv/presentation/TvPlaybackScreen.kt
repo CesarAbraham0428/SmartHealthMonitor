cfd5f729e42235856dbad5a69ea0dff215c2f353
CesarAbraham0428 <lopezcesar0428@gmail.com>
2026-07-11 14:32:41 -0600

    feat(tv): add Navigation Compose with catalog/detail/playback routes

---
+++ tv/src/main/java/mx/utng/smarthealthmonitor/cala/tv/presentation/TvPlaybackScreen.kt
@@ -0,0 +1,9 @@
+package mx.utng.smarthealthmonitor.cala.tv.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun TvPlaybackScreen(navController: NavController) {
+    // Stub
+}
